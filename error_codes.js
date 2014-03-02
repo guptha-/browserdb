80fd72f0ed9d3cc2dcdeaf73faa5faa0e207cb10
svarma1 <svarma@cs.umass.edu>
2014-03-01 21:48:23 -0500

    added new error codes

--- error_codes.js
+++ error_codes.js
@@ -1,9 +1,18 @@
 
-_error_code_dictionary = 
+/* The method below stores the error codes in key value pairs */
 
-{ 
-	0 : "Looks good",
-	1 : "Table name already exists",
-	2 : "Unrecognized query type",
-	3 : "Nested queries not supported by parser"
+function initialize_error_codes()
+{
+    _error_code_dictionary =
+            {
+                0: "Table creation successful",
+                1: "Table name already exists",
+                2: "Unrecognized query type",
+                3: "Nested queries not supported by parser",
+                4: "No. of attributes not equal to no. of values supplied",
+                5: "Table does not exist",
+                6: "Type mismatch between attribute and its value supplied",
+                7: "Given string exceeds set character limit",
+                8: "Tuple successfully inserted into table"
+            };
 }
