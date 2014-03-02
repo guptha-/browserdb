7fd3e701f89afed0b5d0c9f2da209798a189b21c
svarma1 <svarma@cs.umass.edu>
2014-03-01 21:52:00 -0500

    added new string manipulation functions

--- string_manipulation_functions.js
+++ string_manipulation_functions.js
@@ -1,22 +1,63 @@
 
+/* This file has useful functions for string parsing */
 
 function remove_empty_strings(arr)
 {
-	var new_arr = [];
-	var ind = 0;
+    var new_arr = [];
+    var ind = 0;
 
-	for(var j = 0; j < arr.length; j++)
-	{
-	    if(arr[j] !== "")
-	    {
-	        new_arr[ind] = arr[j];
-	        ind = ind + 1;
-	    }   
-	}  
-	return new_arr;
+    for (var j = 0; j < arr.length; j++)
+    {
+        if (arr[j] !== "")
+        {
+            new_arr[ind] = arr[j];
+            ind = ind + 1;
+        }
+    }
+    return new_arr;
 }
 
 function reverse_string(str)
 {
-	return (str.split("").reverse().join(""));
+    return (str.split("").reverse().join(""));
+}
+
+function remove_contiguous_spaces(str)
+{
+    var parts = str.split(" ");
+    var arr = remove_empty_strings(parts);
+    return (arr.join(" "));
+}
+
+function remove_spaces_next_to_special_chars(str)
+{
+    str = str.replace(", ", ",");
+    str = str.replace(" ,", ",");
+
+    str = str.replace(" (", "(");
+    str = str.replace("( ", "(");
+
+    str = str.replace(" )", ")");
+    str = str.replace(") ", ")");
+
+    str = str.replace("' ", "'");
+    str = str.replace(" '", "'");
+
+    str = str.replace("\" ", "\"");
+    str = str.replace(" \"", "\"");
+
+    str = str.replace("; ", ";");
+    str = str.replace(" ;", ";");
+
+
+    return str;
+}
+
+function string_contains(big_string, small_string)
+{
+    if (big_string.indexOf(small_string) !== -1)
+    {
+        return true;
+    }
+    return false;
 }
